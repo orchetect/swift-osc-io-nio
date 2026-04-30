0b1a33cdff58e4265338d53e639ed13b69ebd9a6
Steffan Andrews <stef@orchetect.com>
2026-04-29 18:11:10 -0700

    Added root source file

---
+++ Sources/SwiftOSCIONIO/SwiftOSCIONIO.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftOSCIONIO.swift
+//  SwiftOSC I/O: SwiftNIO • https://github.com/orchetect/swift-osc-io-nio
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+@_documentation(visibility: internal)
+@_exported public import SwiftOSCCore
