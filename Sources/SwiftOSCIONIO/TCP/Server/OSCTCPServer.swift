b286e29bf8c15f27014c967be54ef59cd971b98e
Steffan Andrews <stef@orchetect.com>
2026-05-04 15:10:49 -0700

    `OSCTCPServer`: Fixed issue where server failed to start due to invalid local host address

--- Sources/SwiftOSCIONIO/TCP/Server/OSCTCPServer.swift
+++ Sources/SwiftOSCIONIO/TCP/Server/OSCTCPServer.swift
@@ -111,8 +111,7 @@ extension OSCTCPServer {
                 }
             }
 
-        let host = interface ?? "0.0.0.0"
-        channel = try bootstrap.bind(host: host, port: Int(localPort)).wait()
+        let host = interface ?? "127.0.0.1" // "0.0.0.0"
         channel = try bootstrap
             .bind(host: host, port: Int(_localPort ?? localPort))
             .wait()
