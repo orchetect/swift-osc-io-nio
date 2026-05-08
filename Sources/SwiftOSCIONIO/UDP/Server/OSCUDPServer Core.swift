2a87c78cdb0026c8e83580baab098dcdb5d6c62d
Steffan Andrews <stef@orchetect.com>
2026-05-08 14:56:47 -0700

    `OSCUDPServer.Core`: Added missing deinit

--- Sources/SwiftOSCIONIO/UDP/Server/OSCUDPServer Core.swift
+++ Sources/SwiftOSCIONIO/UDP/Server/OSCUDPServer Core.swift
@@ -51,6 +51,10 @@ extension OSCUDPServer {
             self.queue = queue
             self.receiveHandler = receiveHandler
         }
+        
+        deinit {
+            stop()
+        }
     }
 }
 
