2582e421707822e71d6f6bdd227199783644b57e
Steffan Andrews <stef@orchetect.com>
2026-05-08 23:11:19 -0700

    Updated unit tests

--- Tests/SwiftOSCIOTests/SwiftOSCIOTests.swift
+++ Tests/SwiftOSCIOTests/SwiftOSCIOTests.swift
@@ -6,7 +6,7 @@
 
 // Note:
 //
-// Only tests for specific implementation details of this particular I/O should exist in
+// Only tests for specific implementation details of this particular I/O package should exist in
 // this test target.
 //
 // The standard set of I/O unit tests and integration tests exist in the swift-osc-core repository.
